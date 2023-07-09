e07429d8904cb334fcb9ab36afad3dbb29ee4f2c
kobakoba5884 <kobanminmin58@gmail.com>
2023-07-10 03:32:26 +0900

    revise gitaction folder name

--- packages/backend/src/index.js
+++ packages/backend/src/index.js
@@ -4,19 +4,20 @@ const cors = require("cors");
 
 const main = async () => {
   const BACKEND_PORT = process.env.BACKEND_PORT || 8080;
-  const MONGO_INITDB_ROOT_USERNAME = process.env.MONGO_INITDB_ROOT_USERNAME || 'dbuser'
-  const MONGO_INITDB_ROOT_PASSWORD = process.env.MONGO_INITDB_ROOT_PASSWORD || 'thisIsReallyStrongPassword123'
-  const MONGO_INITDB_DATABASE = process.env.MONGO_INITDB_DATABASE || 'dev'
-  const MONGO_INITDB_COLLECTION = process.env.MONGO_INITDB_COLLECTION || 'test'
-  const MONGO_HOST = process.env.MONGO_HOST || 'mongodb'
-  const MONGO_PORT = process.env.MONGO_PORT || '27017'
+  const MONGO_INITDB_ROOT_USERNAME =
+    process.env.MONGO_INITDB_ROOT_USERNAME || "dbuser";
+  const MONGO_INITDB_ROOT_PASSWORD =
+    process.env.MONGO_INITDB_ROOT_PASSWORD || "thisIsReallyStrongPassword123";
+  const MONGO_INITDB_DATABASE = process.env.MONGO_INITDB_DATABASE || "dev";
+  const MONGO_INITDB_COLLECTION = process.env.MONGO_INITDB_COLLECTION || "test";
+  const MONGO_HOST = process.env.MONGO_HOST || "mongodb";
+  const MONGO_PORT = process.env.MONGO_PORT || "27017";
 
   const app = express();
 
   let db;
 
-  const uri =
-    `mongodb://${MONGO_INITDB_ROOT_USERNAME}:${MONGO_INITDB_ROOT_PASSWORD}@${MONGO_HOST}:${MONGO_PORT}/?authSource=admin&readPreference=primary&ssl=false&directConnection=true`;
+  const uri = `mongodb://${MONGO_INITDB_ROOT_USERNAME}:${MONGO_INITDB_ROOT_PASSWORD}@${MONGO_HOST}:${MONGO_PORT}/?authSource=admin&readPreference=primary&ssl=false&directConnection=true`;
 
   const client = new MongoClient(uri);
 
@@ -36,7 +37,10 @@ const main = async () => {
       return res.send("Waiting for DB connection...");
     }
 
-    const result = await db.collection(MONGO_INITDB_COLLECTION).find().toArray();
+    const result = await db
+      .collection(MONGO_INITDB_COLLECTION)
+      .find()
+      .toArray();
     res.send(result);
   });
 
