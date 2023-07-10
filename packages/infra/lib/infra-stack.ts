eb6eb23f0c91db192eb42436d7c897e0e28b0062
kobakoba5884 <kobanminmin58@gmail.com>
2023-07-11 00:09:31 +0900

    add infra project with aws cdk

---
+++ packages/infra/lib/infra-stack.ts
@@ -0,0 +1,74 @@
+import * as cdk from "aws-cdk-lib";
+import {
+  Peer,
+  Port,
+  SecurityGroup,
+  SubnetType,
+  Vpc,
+} from "aws-cdk-lib/aws-ec2";
+import { Construct } from "constructs";
+
+export class InfraStack extends cdk.Stack {
+  private readonly PROJECT_NAME = "hello-world-app";
+  private readonly BACKEND_PORT = 8080;
+  private readonly FRONTEND_PORT = 80;
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    const vpc = this.createVpc();
+    const sg = this.createSgForALB(vpc);
+
+    this.createOutputs(vpc, sg);
+  }
+
+  createVpc = (): Vpc => {
+    const vpcName = `${this.PROJECT_NAME}-vpc`;
+    return new Vpc(this, vpcName, {
+      vpcName: vpcName,
+      enableDnsHostnames: true,
+      enableDnsSupport: true,
+      maxAzs: 2,
+      natGateways: 0,
+      subnetConfiguration: [
+        {
+          cidrMask: 24,
+          name: `${this.PROJECT_NAME}-private-subnet`,
+          subnetType: SubnetType.PRIVATE_ISOLATED,
+        },
+        {
+          cidrMask: 24,
+          name: `${this.PROJECT_NAME}-public-subnet`,
+          subnetType: SubnetType.PUBLIC,
+        },
+      ],
+    });
+  };
+
+  createSgForALB = (vpc: Vpc): SecurityGroup => {
+    const sgName = `${this.PROJECT_NAME}-sg-for-alb`;
+
+    const sg = new SecurityGroup(this, sgName, {
+      vpc: vpc,
+      description: "Allow inbound traffic from ALB on ports 80 and 8080",
+      allowAllOutbound: true,
+      securityGroupName: sgName,
+    });
+
+    sg.addIngressRule(Peer.anyIpv4(), Port.tcp(this.FRONTEND_PORT));
+    sg.addIngressRule(Peer.anyIpv4(), Port.tcp(this.BACKEND_PORT));
+
+    return sg;
+  };
+
+  createOutputs = (vpc: Vpc, sg: SecurityGroup) => {
+    new cdk.CfnOutput(this, "vpcId", {
+      description: "The id of the Vpc",
+      value: vpc.vpcId,
+    });
+
+    new cdk.CfnOutput(this, "sgId", {
+      description: "The id of the Security Group",
+      value: sg.securityGroupId,
+    });
+  };
+}
